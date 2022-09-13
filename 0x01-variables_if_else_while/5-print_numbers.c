0190cc08e8cac975971ce32030f23da9057115d3
Felixheavenz <Felixheavenzall@gmail.com>
2022-09-13 05:20:06 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,33 @@
+/**
+ *make - initiates the program
+ *
+ *Description: conditional statement
+ *Return: Always 0 (success)
+ **/
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+int main(void)
+{
+
+	int a;
+
+	/* your code goes there */
+
+	for (a = 0; a < 10; a++)
+
+	{
+
+		printf("%d", a);
+
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
