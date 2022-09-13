3cbd7e6e59d475c007a326f5d356f4c68ba5a720
Felixheavenz <Felixheavenzall@gmail.com>
2022-09-13 05:38:15 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,36 @@
+/**
+  *main - initiates the program
+  *Description: conditional statement
+  *
+  *Return: Always 0 (success)
+  *
+  **/
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+int main(void)
+
+{
+
+	int ch = 0;
+
+	/* your code goes there */
+
+	while (ch < 10)
+
+	{
+
+		putchar (ch + '0');
+
+		ch++;
+
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
