16e2483e7bd760fa2d59e577c3c9fbf961eeaacd
Felixheavenz <Felixheavenzall@gmail.com>
2022-09-16 20:41:51 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,16 +7,15 @@
 *Return: returns nothing
 */
 
-void print_square(int sze)
+void print_square(int size)
 {
-	int inc1, inc2;
+	int inc, inc2;
 
 	if (size > 0)
 	{
 		for (inc1 = 0; inc1 < size; inc1++)
 		{
 			for (inc2 = 0; inc2 < (size - 1); inc2++)
-		
 			{
 				putchar('#');
 			}
