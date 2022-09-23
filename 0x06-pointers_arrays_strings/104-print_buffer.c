921384554acbc50b0218b6e07cded30cecf65bd4
Felixheavenz <Felixheavenzall@gmail.com>
2022-09-22 23:30:01 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -6,31 +6,29 @@
  * @c: buffer to print
  * @s: bytes of buffer to print
  * @l: line of buffer to print
- * 
  * Return: void
  */
 
 void print_line(char *c, int s, int l)
 {
 	int j, k;
-	
+
 	for (j = 0; j <= 9; j++)
 	{
 		if (j <= s)
-			printf("%02x", c[l * 10 + j]);						else
+			printf("%02x", c[l * 10 + j]);
+		else
 			printf("  ");
-
 		if (j % 2)
 			putchar(' ');
-											}
+	}
 	for (k = 0; k <= s; k++)
 	{
 		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-
 			putchar(c[l * 10 + k]);
 		else
-
-			putchar('.');							}
+			putchar('.');
+	}
 }
 
 /**
@@ -40,11 +38,10 @@ void print_line(char *c, int s, int l)
  *
  * Return: void
  */
-
 void print_buffer(char *b, int size)
 {
 	int i;
-	
+
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
 		printf("%08x: ", i * 10);
@@ -57,7 +54,6 @@ void print_buffer(char *b, int size)
 			print_line(b, size % 10 - 1, i);
 		}
 		putchar('\n');
-	
 	}
 	if (size == 0)
 		putchar('\n');
