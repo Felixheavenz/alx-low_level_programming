97aebcd8c8f2ee7f10f2544813139513e31ffc8e
Felixheavenz <Felixheavenzall@gmail.com>
2022-09-27 09:32:49 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,7 +3,7 @@
 /**
  * _strstr - locates a substring
  * @haystack: string in which to check for needle
- * @neddle: substring to find in haystack
+ * @needle: substring to find in haystack
  *
  * Return: pointer to beginning of needle in haystack or NULL if no match
  */
