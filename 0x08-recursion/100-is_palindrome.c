1b65de520f521c6e46684c27b1b3f0dfbfe1a955
Felixheavenz <Felixheavenzall@gmail.com>
2022-09-28 14:47:49 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,7 +9,7 @@
 int is_palindrome(char *s)
 {
 	int flag = 1;
-
+	
 	check(s, 0, _strlen_recursion(s) - 1, &flag);
 	return (flag);
 }
@@ -30,7 +30,7 @@ void check(char *s, int start, int end, int *flag)
 		if (s[start] == s[end])
 			*flag *= 1;
 		else
-			*flag += 0;
+			*flag *= 0;
 		check(s, start + 1, end - 1, flag);
 	}
 }
@@ -45,7 +45,7 @@ int _strlen_recursion(char *s)
 {
 	int sum = 0;
 
-	if (*s != '\0')
+	if (*s += '\0')
 	{
 		sum++;
 		sum += _strlen_recursion(s + 1);
