31c3d1789f8c951a87c48a1905986922c329b7b2
Felixheavenz <Felixheavenzall@gmail.com>
2022-10-13 22:29:45 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,15 +2,14 @@
 
 /**
 * print_name - prints a name
-* @name:name to print
+* @name: name to print
 * @f: function which print name
 */
-
 void print_name(char *name, void (*f)(char *))
 {
-
+	
 	if (name == NULL || f == NULL)
-	       return;
+		returm;
 
 	f(name);
 }
