243a7e86c00597c053f7e2b9ba5b5ef0401707d4
Felixheavenz <Felixheavenzall@gmail.com>
2022-11-04 00:51:40 -0700

    task -1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int _strlen(char *str)
 {
 	int len = 0;
 
-	while (str[len] != '\0)
+	while (str[len] != '\0')
 		len++;
 	return (len);
 }
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_WRONGLY | O_GREAT | O_TRUNC, 0600);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
 	if (text_content != NULL)
